4304e70b22ad39c7c743ef29cc05df1fad8ed4ad
Yegana Hasanova <yeganamha@code.edu.az>
2023-06-02 09:26:45 -0700

    task4

---
+++ MathHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class MathHelper
+    {
+        public int PrintValue(int a, int b)
+        { 
+            return
+                a + b;
+        }
+
+        public int PrintValue(bool c) 
+        {
+            if (c = true) ;
+            return 1;
+          
+             
+        }
+        public void PrintValue(string str1, string str2)
+        {
+            Console.WriteLine( str1+str2);
+        }
+
+        public int PrintValue(int d, int p, int f)
+        {
+            return
+                d * p * f;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathHelper mathHelper = new MathHelper();
+
+            Console.WriteLine(mathHelper.PrintValue(4, 5));
+            Console.WriteLine(mathHelper.PrintValue(true));
+            Console.WriteLine(mathHelper.PrintValue(4, 5, 2));
+
+            mathHelper.PrintValue("Yegana", "Hasanova");
+
+
+        }
+    }
+}
